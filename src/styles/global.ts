52b2fb477b4420b1febaa7cd619a0ea8fba8470f
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-12-30 12:21:15 -0300

    style: add color props

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,9 +7,9 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
   body {
-    background: #f6f6f6;
+    background: ${props => props.theme.colors.background};
     font-size: 14px;
-    color: black;
+    color: ${props => props.theme.colors.text};
     font-family: sans-serif;
   }
 `;
