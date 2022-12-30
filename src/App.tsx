cbb5629db7c5d3f3f9e2a8da8ad28678d04a26c6
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-12-30 12:18:44 -0300

    feat: import styles and hooks for header/darkmode_switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { DefaultTheme, ThemeProvider } from 'styled-components';
+import Header from './components/Header';
 import GlobalStyle from './styles/global';
+import dark from './styles/themes/dark';
+import light from './styles/themes/light';
+import usePersistedState from './utils/usePersistedState';
 
 function App() {
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === 'light' ? dark : light);
+  }
+
   return (
-    <div className="App">
-      <GlobalStyle />
-      <>
-        "Hello World"
-      </>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <GlobalStyle />
+        <Header toggleTheme={toggleTheme} />
+      </div>
+    </ThemeProvider>
   );
 }
 
