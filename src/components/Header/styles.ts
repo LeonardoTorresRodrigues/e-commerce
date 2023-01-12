2ce7659ee13005d16e5105161f16b3b6605875ef
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-01-12 12:45:21 -0300

    style: banner text style

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -27,7 +27,7 @@ export const Login = styled.button`
   height: 51px;
   padding: 16px;
   gap: 10px;
-  border: 1px solid ${props => props.theme.colors.secondary};
+  border: none;
   border-radius: 20px;
   color: ${props => props.theme.colors.primary};
   cursor: pointer;
@@ -53,4 +53,31 @@ export const BannerText = styled.div`
   color:#FFFFFF;
   width:50%;
   margin: 10rem 0;
+  gap: 1rem;
+
+  h1 {
+    font-size: 3.25rem;
+    line-height: 31px;
+    font-weight: 700;
+  }
+
+  p {
+    font-size: 1.375rem;
+    line-height: 26px;  
+    font-weight: 700;  
+  }
+
+  button {
+    width: 8.125rem;
+    height: 3.188rem;
+    gap: 0.625rem;
+    padding: 1rem;
+    background-color: ${props => props.theme.colors.primary};
+    color: #f6f6f6;
+    font-size: 1rem;
+    line-height: 19px;
+    font-weight: 400;
+    border: none;
+    cursor: pointer;
+  }
   `;
